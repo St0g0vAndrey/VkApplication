929d1a80de9a1116b40e5bf092be1000567e44af
St0g0vAndrey <stogovand@icloud.com>
2021-12-20 22:08:02 +1000

    Vk Application

---
+++ VkApplication/ViewControllers/LoginViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  VkApplication
+//
+//  Created by Андрей Стогов on 18.12.2021.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+/*
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var passwTextField: UITextField!
+    
+    @IBAction func ButtonProced (_ sender: Any) {
+    }
+    */
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+    }
+
+}
+
