8cce221c8ecc62e0c4a21138fb8952c02dea92c6
St0g0vAndrey <stogovand@icloud.com>
2021-12-22 18:25:39 +1000

    vk

--- VkApplication/Source/AppDelegate.swift
+++ VkApplication/Source/AppDelegate.swift
@@ -4,19 +4,19 @@
 //
 //  Created by Андрей Стогов on 18.12.2021.
 //
-
+import SwiftUI
 import UIKit
 
+@main
+
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
@@ -29,7 +29,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
 
