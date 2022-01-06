6914173adbaae319f4bb865fcd3724cfca2a534e
St0g0vAndrey <stogovand@icloud.com>
2022-01-06 22:23:34 +1000

    VK APP

--- VkApplication/ViewControllers/MyGroupsTableVC.swift
+++ VkApplication/ViewControllers/MyGroupsTableVC.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-final class MyGroupsTableVC: UITableViewController {
+class MyGroupsTableVC: UITableViewController {
   
     // MARK: - Life Cycle
     override func viewDidLoad() {
