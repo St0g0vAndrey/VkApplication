edc29279b2539ef497f8d889ff93978ce3762fb0
St0g0vAndrey <stogovand@icloud.com>
2022-01-22 00:22:27 +1000

    vk

--- VkApplication/View/Resoul Cell/CollectionCell/PhotoCollectionCell.swift
+++ VkApplication/View/Resoul Cell/CollectionCell/PhotoCollectionCell.swift
@@ -9,41 +9,48 @@ import UIKit
 
 final class PhotoCollectionCell: UICollectionViewCell {
 
+    var numbertLike: Int = 0
+    var isLicked: Bool = false
+    
+    
     @IBOutlet weak var buttonLike: UIButton!
     @IBOutlet weak var countLike: UILabel!
     @IBOutlet weak var photoCollection: UIImageView!
+    @IBAction func clickToLike(_ sender: Any) {
+        
+        switch buttonLike.isEnabled {
+        case true:
+            buttonLike.setImage(UIImage(systemName: "heart.fill"), for: .normal)
+            
+            if (numbertLike == 0) {
+                numbertLike += 1
+                countLike.text = String(numbertLike)
+            } else {
+                numbertLike = 0
+                countLike.text = String(numbertLike)
+                buttonLike.setImage(UIImage(systemName: "heart"), for: .normal)
+            }
+        case false:
+         //  buttonLike.setImage(UIImage(systemName: "heart"), for: .normal)
+            break
+            
+        }
+    }
     
     //MARK: - Cicle
     override func awakeFromNib() {
         super.awakeFromNib()
         configLabel()
-        buttonAction()
     }
     
     //MARK: - Init
     func configure(photo: UIImage?) {
         self.photoCollection.image = photo
     }
-    
-    private func buttonAction() {
-        switch buttonLike.isEnabled {
-        case true:
-            buttonLike.setImage(UIImage(systemName: "heart.fill"), for: .highlighted)
-            count += 1
-            countLike.text = String(count)
-        case false:
-            buttonLike.setImage(UIImage(systemName: "heart"), for: .disabled)
-            
-        }
-    }
-    
-    
-    var count = 0
+
     private func configLabel () {
-        
         countLike.textColor = .red
-        countLike.text = String(count)
-        //countLike.sizeThatFits(CGSize(width: 15.0, height: 20.0))
+        countLike.text = String(numbertLike)
     }
 
     
