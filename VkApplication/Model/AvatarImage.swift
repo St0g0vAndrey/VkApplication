3acae5ad16033f13c636fdca28b7358d29ad63d5
St0g0vAndrey <stogovand@icloud.com>
2022-01-21 20:53:22 +1000

    vk

--- VkApplication/Model/AvatarImage.swift
+++ VkApplication/Model/AvatarImage.swift
@@ -9,54 +9,50 @@ import UIKit
 
 class AvatarImage: UIImageView {
 
-    @IBInspectable var borderColor: UIColor = .darkGray
-    @IBInspectable var borderWidth: CGFloat = 2.0
+    @IBInspectable var borderColor: UIColor = .green
+    @IBInspectable var borderWidth: CGFloat = 1.0
+    @IBInspectable var shadowColor: UIColor = .red
+    @IBInspectable var shadowOffset: CGSize = CGSize(width: 3.0, height: 3.0)
+    @IBInspectable var shadowOpacity: Float = 0.8
+    @IBInspectable var shadowRadius: CGFloat = 10
+    @IBInspectable var shadowBlur: CGFloat = 20.0
     
     override func awakeFromNib() {
         self.layer.cornerRadius = self.frame.width / 2
         self.layer.masksToBounds = true
         self.layer.borderWidth = borderWidth
         self.layer.borderColor = borderColor.cgColor
-    }
-}
-
-class AvatarShadow: UIView {
-    
-    @IBInspectable var shadowColor: UIColor = .black
-    @IBInspectable var shadowOffset: CGSize = CGSize(width: 0.0, height: -3)
-    @IBInspectable var shadowOpacity: Float = 0.8
-    @IBInspectable var shadowRadius: CGFloat = 10
-    
-    override func awakeFromNib() {
-        self.backgroundColor = .clear
+        self.backgroundColor = .black
         self.layer.shadowColor = shadowColor.cgColor
         self.layer.shadowOffset = shadowOffset
         self.layer.shadowOpacity = shadowOpacity
         self.layer.shadowRadius = shadowRadius
     }
     
-    var logoView = UIImageView()
+    var emblemView = UIImageView()
     let shadowView = UIView()
     
     override func draw(_ rect: CGRect) {
         super.draw(rect)
         
-        logoView.frame = rect
-        logoView.layer.cornerRadius = shadowRadius
-        logoView.clipsToBounds = true
-        logoView.transform = CGAffineTransform(scaleX: 0.9, y: 0.9)
+        emblemView.frame = rect
+        emblemView.layer.cornerRadius = shadowRadius
+        emblemView.clipsToBounds = true
+        emblemView.transform = CGAffineTransform(scaleX: 2.0, y: 2.0)
 
         shadowView.frame = rect
         shadowView.clipsToBounds = false
         shadowView.layer.shadowColor = shadowColor.cgColor
         shadowView.layer.shadowOpacity = shadowOpacity
         shadowView.layer.shadowOffset = shadowOffset
-        //shadowView.layer.shadowRadius = shadowBlur
-        shadowView.layer.shadowPath = UIBezierPath(
-            roundedRect: shadowView.bounds,
-            cornerRadius: shadowRadius).cgPath
-        
-        shadowView.addSubview(logoView)
+        shadowView.layer.shadowRadius = shadowBlur
+        shadowView.addSubview(emblemView)
         self.addSubview(shadowView)
     }
+    
+    
 }
+
+
+    
+   
