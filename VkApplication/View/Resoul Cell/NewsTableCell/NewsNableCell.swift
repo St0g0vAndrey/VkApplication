9ad8a51cb984f91d8e82896e27b7af8257752614
St0g0vAndrey <stogovand@icloud.com>
2022-01-30 13:10:33 +1000

    vk

---
+++ VkApplication/View/Resoul Cell/NewsTableCell/NewsNableCell.swift
@@ -0,0 +1,22 @@
+//
+//  NewsNableCell.swift
+//  VkApplication
+//
+//  Created by Андрей Стогов on 30.01.2022.
+//
+
+import UIKit
+
+class NewsNableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
