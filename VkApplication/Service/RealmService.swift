2c0ce941ca319b98a5ea449027f83ec77c50c8a1
Andrey Stogov <stogovand@icloud.com>
2022-04-22 01:15:12 +1000

    vk

--- VkApplication/Service/RealmService.swift
+++ VkApplication/Service/RealmService.swift
@@ -5,4 +5,36 @@
 //  Created by Андрей Стогов on 22.04.2022.
 //
 
-import Foundation
+import RealmSwift
+
+final class RealmService {
+    static let deleteIfMigration = Realm.Configuration(deleteRealmIfMigrationNeeded: true)
+    
+    class func save<T:Object>(
+        items: [T],
+        configuration: Realm.Configuration = deleteIfMigration,
+        update: Realm.UpdatePolicy = .modified) throws {
+            
+            let realm = try Realm(configuration: configuration)
+            print(configuration.fileURL ?? "")
+            try realm.write{
+                realm.add(
+                    items,
+                    update: update)
+            }
+        }
+    
+    
+    class func load<T: Object>(typyof: T.Type) throws -> Results<T>{
+        let realm = try Realm()
+        return realm.objects(T.self)
+    }
+    
+    class func delete<T:Object>(obj: Results<T>) throws {
+        let realm = try Realm()
+        try realm.write {
+            realm.delete(obj)
+        }
+    }
+    
+}
